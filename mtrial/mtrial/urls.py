66a6ebb658cc830099a671f72c3048f418ffae9e
Andrew <barker@bluewin.ch>
2022-06-03 13:28:24 +0200

    pairings shown all views

--- mtrial/mtrial/urls.py
+++ mtrial/mtrial/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('itasc.urls', namespace='dashboard')),
     path('itasc/', include('itasc.urls', namespace='itasc')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
